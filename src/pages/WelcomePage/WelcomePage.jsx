1dec17b695f73feb9dfcd4b4cd4880e2b85f5e6f
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-11-06 12:51:49 +0100

    06112024

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,10 +1,67 @@
 import { SliderWelcome } from "../../components/Slider/SliderWelcome";
-
+import {
+  BoxContainer,
+  BoxHeader,
+  ImageBox,
+  WelcomeContainer,
+  WelcomeGeneral,
+  WelcomeItem,
+  WelcomeList,
+} from "./WelcomePage.styled";
+import Box from "./box.png";
+import Gold from "./gold.png";
+import Set from "./set.png";
+import Silver from "./silver.png";
 export const WelcomePage = () => {
   return (
     <div>
       <SliderWelcome />
-      <h1>Dziękuję że wybrałeś Nas</h1>
+      <WelcomeGeneral>Katalog</WelcomeGeneral>
+      <WelcomeContainer>
+        <WelcomeList>
+          <WelcomeItem>
+            {/* link */}
+            <BoxContainer>
+              <ImageBox
+                src={Gold}
+                alt="gold earrings"
+                style={{ width: "220px" }}
+              />
+              <BoxHeader> Chirurgiczne ZŁOTO</BoxHeader>
+            </BoxContainer>
+          </WelcomeItem>
+          <WelcomeItem>
+            <BoxContainer>
+              <ImageBox
+                src={Silver}
+                alt="silver earrings"
+                style={{ width: "220px" }}
+              />
+              <BoxHeader> Chirurgiczne ŚREBRO</BoxHeader>
+            </BoxContainer>
+          </WelcomeItem>
+          <WelcomeItem>
+            <BoxContainer>
+              <ImageBox
+                src={Set}
+                alt="set gold and silver earrings"
+                style={{ width: "220px" }}
+              />
+              <BoxHeader>Nabory</BoxHeader>
+            </BoxContainer>
+          </WelcomeItem>
+          <WelcomeItem>
+            <BoxContainer>
+              <ImageBox
+                src={Box}
+                alt="box on the gold and silver earrings"
+                style={{ width: "220px" }}
+              />
+              <BoxHeader> Pakowanie prezentów</BoxHeader>
+            </BoxContainer>
+          </WelcomeItem>
+        </WelcomeList>
+      </WelcomeContainer>
     </div>
   );
 };
---
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+export const WelcomeGeneral = styled.h1`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  font-family: "Noto Sans", sans-serif;
+  font-size: 42px;
+  font-weight: 700;
+  color: gray;
+  text-shadow: 0 0 5px rgb(167, 182, 208);
+`;
+export const WelcomeContainer = styled.div`
+  padding: 20px 190px;
+`;
+export const WelcomeList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  row-gap: 100px;
+  gap: 70px;
+`;
+export const WelcomeItem = styled.li`
+  border: 1px solid lightgray;
+  padding: 20px;
+`;
+export const ImageBox = styled.img`
+  // width: 100%;
+  // height: auto;
+  // max-height: 90%;
+  object-fit: contain;
+  margin-bottom: 15px;
+`;
+export const BoxContainer = styled.div`
+  height: 250px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transform: scale(1.05);
+`;
+export const BoxHeader = styled.h2`
+  font-family: "Nunito", sans-serif;
+  color: darkred;
+  font-weight: 700;
+  margin-top: auto;
+  text-align: center;
+  text-shadow: 0 0 5px rgb(167, 182, 208);
+`;
