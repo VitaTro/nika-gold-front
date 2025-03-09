de76e474db7c317cb251097cf25b2a05f4de3008
VitaTro <144832571+VitaTro@users.noreply.github.com>
2025-03-09 17:31:26 +0100

    2

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -71,8 +71,8 @@ export const Slider = styled.div`
     content: "";
     height: 22px;
     width: 22px;
-    left: ${(props) => (props.theme.isDarkMode ? "30px" : "4px")};
-    right: ${(props) => (props.theme.isDarkMode ? "4px" : "30px")};
+    left: ${(props) => (props.theme.$isDarkMode ? "30px" : "4px")};
+    right: ${(props) => (props.theme.$isDarkMode ? "4px" : "30px")};
     bottom: 4px;
     background-color: #333;
     transition: 0.6s;
